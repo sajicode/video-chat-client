251e02bc0f9055de8897b6eb0a83d4b202ec7046
sajicode <sajioloye@gmail.com>
2020-08-08 17:41:14 +0100

    link static files to server

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ io.on('connection', socket => {
 
 });
 
+//* serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
 const PORT = process.env.PORT || 2020;
 
 server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
